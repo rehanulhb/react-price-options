e0fb541f3f45b8bef5abab9f963f6232c61006f7
rehanulhb <rehanulh@gmail.com>
2024-11-13 02:50:35 +0100

    Working on NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
 import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   
 
   return (
     <>
-      
+      {/* <DaisyNav></DaisyNav> */}
+      <NavBar></NavBar>
       <h1 className='text-7xl bg-rose-500'>Vite + React</h1>
+
+      
       
       
     </>
