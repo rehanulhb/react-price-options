1beb9c08c5d8bb62689813e81298c1d14a2e2308
rehanulhb <rehanulh@gmail.com>
2024-11-16 02:12:47 +0100

    Showing price options to UI and give them some styles

--- src/components/PriceOption/PriceOption.jsx
+++ src/components/PriceOption/PriceOption.jsx
@@ -1,14 +1,26 @@
 import PropTypes from 'prop-types';
+import Feature from '../Feature/Feature';
 
 const PriceOption = ({option}) => {
+    const {option_name, price, features} = option;
     return (
-        <div>
-            
+        <div className='bg-blue-300 rounded-md p-4 flex flex-col text-white '>
+            <h2 className='text-center'>
+                <span className='text-7xl font-extrabold'> {price}</span>
+                <span className='text-3xl'>/mon</span>
+            </h2>
+            <h4 className='text-5xl text-center my-8'>{option_name}</h4>
+            <div className='pl-6 flex-grow'>
+            {
+                features.map((feature, index)=><Feature key={index} feature={feature} ></Feature>)
+            }
+            </div>
+            <button className='mt-12 bg-green-600 w-full py-2 font-bold rounded-lg hover:bg-green-900'>Buy Now</button>
         </div>
     );
 };
 
-PriceOption.propTypes ={
+PriceOption.propTypes = {
     option: PropTypes.object
 }
 
--- src/components/PriceOptions/PriceOptions.jsx
+++ src/components/PriceOptions/PriceOptions.jsx
@@ -1,94 +1,93 @@
 import PriceOption from "../PriceOption/PriceOption";
 
-
 const PriceOptions = () => {
+  const priceOptions = [
+    {
+      id: 1,
+      option_name: "Basic",
+      price: 29.99,
+      features: [
+        "Access to gym equipment",
+        "Locker room access",
+        "One free fitness assessment",
+        "Free Wi-Fi",
+        "Access to mobile app",
+        "One guest pass per month",
+      ],
+    },
+    {
+      id: 2,
+      option_name: "Standard",
+      price: 49.99,
+      features: [
+        "Access to gym equipment",
+        "Locker room access",
+        "Five free fitness assessments",
+        "Access to group fitness classes",
+        "One personal training session per month",
+        "Free Wi-Fi",
+        "Access to mobile app",
+        "Two guest passes per month",
+        "Access to nutrition and wellness workshops",
+      ],
+    },
+    {
+      id: 3,
+      option_name: "Premium",
+      price: 79.99,
+      features: [
+        "Access to gym equipment",
+        "Locker room access",
+        "Unlimited fitness assessments",
+        "Access to all group fitness classes",
+        "Four personal training sessions per month",
+        "Access to sauna and steam room",
+        "Guest passes",
+        "Free Wi-Fi",
+        "Access to mobile app",
+        "Priority support",
+        "Access to nutrition and wellness workshops",
+        "Access to massage chairs",
+        "Discounts on gym merchandise",
+      ],
+    },
+    {
+      id: 4,
+      option_name: "VIP",
+      price: 99.99,
+      features: [
+        "Access to gym equipment",
+        "Locker room access",
+        "Unlimited fitness assessments",
+        "Access to all group fitness classes",
+        "Eight personal training sessions per month",
+        "Access to sauna, steam room, and pool",
+        "Unlimited guest passes",
+        "Priority booking for classes and equipment",
+        "Free gym merchandise",
+        "Free Wi-Fi",
+        "Access to mobile app",
+        "Priority support",
+        "Access to nutrition and wellness workshops",
+        "Access to massage chairs",
+        "Complimentary towel service",
+        "Reserved parking spot",
+        "Personalized fitness plan",
+        "Monthly fitness progress reports",
+      ],
+    },
+  ];
 
-    const priceOptions = [
-        {
-            "id": 1,
-            "option_name": "Basic",
-            "price": 29.99,
-            "features": [
-                "Access to gym equipment",
-                "Locker room access",
-                "One free fitness assessment",
-                "Free Wi-Fi",
-                "Access to mobile app",
-                "One guest pass per month"
-            ]
-        },
-        {
-            "id": 2,
-            "option_name": "Standard",
-            "price": 49.99,
-            "features": [
-                "Access to gym equipment",
-                "Locker room access",
-                "Five free fitness assessments",
-                "Access to group fitness classes",
-                "One personal training session per month",
-                "Free Wi-Fi",
-                "Access to mobile app",
-                "Two guest passes per month",
-                "Access to nutrition and wellness workshops"
-            ]
-        },
-        {
-            "id": 3,
-            "option_name": "Premium",
-            "price": 79.99,
-            "features": [
-                "Access to gym equipment",
-                "Locker room access",
-                "Unlimited fitness assessments",
-                "Access to all group fitness classes",
-                "Four personal training sessions per month",
-                "Access to sauna and steam room",
-                "Guest passes",
-                "Free Wi-Fi",
-                "Access to mobile app",
-                "Priority support",
-                "Access to nutrition and wellness workshops",
-                "Access to massage chairs",
-                "Discounts on gym merchandise"
-            ]
-        },
-        {
-            "id": 4,
-            "option_name": "VIP",
-            "price": 99.99,
-            "features": [
-                "Access to gym equipment",
-                "Locker room access",
-                "Unlimited fitness assessments",
-                "Access to all group fitness classes",
-                "Eight personal training sessions per month",
-                "Access to sauna, steam room, and pool",
-                "Unlimited guest passes",
-                "Priority booking for classes and equipment",
-                "Free gym merchandise",
-                "Free Wi-Fi",
-                "Access to mobile app",
-                "Priority support",
-                "Access to nutrition and wellness workshops",
-                "Access to massage chairs",
-                "Complimentary towel service",
-                "Reserved parking spot",
-                "Personalized fitness plan",
-                "Monthly fitness progress reports"
-            ]
-        }
-    ];
-    
-
-    return (
-        <div>
-            <h2 className="text-5xl">Best Prices</h2>
-            {
-                priceOptions.map(option => <PriceOption key={option.id}></PriceOption>)
-            }
-        </div>
-    );
+  return (
+    <div className="m-12">
+      <h2 className="text-5xl">Best Prices</h2>
+      <div className="grid md:grid-cols-3 gap-6">
+        {priceOptions.map((option) => (
+          <PriceOption option={option} key={option.id}></PriceOption>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PriceOptions;
